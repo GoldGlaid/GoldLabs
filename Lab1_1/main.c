504c1b82a631a97297e74a8bc83e37b87f92acb1
GoldGlaid <91071544+GoldGlaid@users.noreply.github.com>
2024-09-27 12:00:35 +0300

    Update main.c
    add errors if's

--- Lab1_1/main.c
+++ Lab1_1/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+typedef enum STATE_ERRORS ( OK, INVALID INPUT, INVALID FLAG, INVALID NUMBER);
+
+//TODO сделать функцию на проверку строки как число
+int is_digit(const char * str_num, int base) {
 }
+
+int main(int argc, char *argv[]) {
+
+    // проверка на кол-во аргументов
+    if (argc != 3) {
+        printf("ERROR: INVALID INPUT");
+        return INVALID INPUT;
+    }
+    // проверка на верность введённого флага
+    if (!(argv[1][0] == '-' || argv[1][0] == '/') && argv[1][2] == '\0') {
+        printf("ERROR: INVALID FLAG");
+        return INVALID FLAG;
+    }
+    // проверка на верность введённого числа
+    if (!is_digit(argv[2], 10)) {
+        printf("ERROR: INVALID NUMBER");
+        return INVALID NUMBER;
+    }
+}
+
+
