d64ca0ffd78fe005ee08b8d8eb0cac530848672d
GoldGlaid <isavckov@yandex.ru>
2024-10-11 00:06:56 +0300

    Beta_release task#4

--- Lab1/task_4/task4.c
+++ Lab1/task_4/task4.c
@@ -1,92 +1,87 @@
-#include <limits.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-#define BUFFER_SIZE 100
 
 typedef enum STATE_ERRORS {
     OK,
     INVALID_INPUT = -1,
     INVALID_FLAG = -2,
-    INVALID_NUMBER = -3,
-    ERROR_OVERFLOW = -4,
-    ZERO_NUMBER = -5,
-    NEGATIVE_NUMBER = -6,
     NOT_EXIST_FILE = -7,
+    INVALID_FILE = -8,
+    MEMORY_ERROR = -9,
 } STATE_ERRORS;
 
-// Функция для перевода из строчного представления в числовое (целое), проверка некоторых условий
-long long int str_to_int(const char *argv, long long int *x) {
-    char *end_p;
-    long long num = strtoll(argv, &end_p, 10);
 
-    if (num >= LLONG_MAX || num <= LLONG_MIN) {
-        return INVALID_NUMBER;
+void remove_numbers(FILE *input, FILE *output) {
+    int symbol;
+    while ((symbol = fgetc(input)) != EOF) {
+        if (symbol >= '0' && symbol <= '9') {
+            continue;
+        }
+        fputc(symbol, output);
     }
-    if (*end_p != '\0') {
-        return INVALID_NUMBER;
-    }
-    *x = num;
-    return OK;
 }
 
-// Функция для перевода из строчного представления в числовое (вещественное), проверка некоторых условий
-int str_to_long_double(const char *str, double *result) {
-    int dot_flag = 0;
+void count_roman_alphabet(FILE *input, FILE *output) {
+    int symbol;
+    long long int counter_symbols_RA = 0, counter_rows = 1;
 
-    for (int i = 0; i < strlen(str); ++i) {
-        if ((str[i] < '0' || str[i] > '9') && str[i] != '.' && str[i] != '-') {
-            return INVALID_NUMBER;
-        }
-        if (str[i] == '-' && i != 0) {
-            return INVALID_NUMBER;
-        }
-        if (str[i] == '.') {
-            dot_flag++;
-        }
-    }
-    if (dot_flag != 1) {
-        return INVALID_NUMBER;
-    }
+    fprintf(output, "%lld) ", counter_rows);
+    while ((symbol = fgetc(input)) != EOF) {
+        if (symbol == '\n') {
+            fprintf(output, "%lld\n", counter_symbols_RA);
+            counter_symbols_RA = 0;
 
-    char *end_p;
-    double num = strtod(str, &end_p);
-
-    if (num == HUGE_VAL || num == -HUGE_VAL) {
-        return INVALID_NUMBER;
+            counter_rows++;
+            fprintf(output, "%lld) ", counter_rows);
+        } else {
+            if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')) {
+                counter_symbols_RA++;
+            }
+        }
     }
-    if (num == 0 && end_p == str) {
-        return INVALID_NUMBER;
-    }
-    if (*end_p != '\0') {
-        return INVALID_NUMBER;
-    }
-    *result = num;
-    return OK;
+    fprintf(output, "%lld\n", counter_symbols_RA);
 }
 
-//TODO Обработать если входящий файл == выходящему файлу
-int flag_D(char *argv[], bool n) {
 
-    FILE *input_file = fopen(argv[2], "r");
-    if (n) {
-        FILE *output_file = fopen(argv[3], "r");
-    } else {
-        char
+void count_specific_symbols(FILE *input, FILE *output) {
+    int symbol;
+    long long int counter_symbols_RA = 0, counter_rows = 1;
 
+    fprintf(output, "%lld) ", counter_rows);
+    while ((symbol = fgetc(input)) != EOF) {
+        if (symbol == '\n') {
+            counter_symbols_RA++;
+            fprintf(output, "%lld\n", counter_symbols_RA);
+            counter_symbols_RA = 0;
+
+            counter_rows++;
+            fprintf(output, "%lld) ", counter_rows);
+        } else {
+            if (!((symbol >= 'A' && symbol <= 'Z') ||
+                  (symbol >= 'a' && symbol <= 'z') ||
+                  (symbol >= '0' && symbol <= '9') || symbol == ' ')) {
+                counter_symbols_RA++;
+            }
+        }
     }
-    if (input_file == NULL) {
-        fclose(input_file);
-        return NOT_EXIST_FILE;
-    }
-
+    counter_symbols_RA++;
+    fprintf(output, "%lld\n", counter_symbols_RA);
 }
 
-//name -h path
-//name -nh path outpath
+
+void replace_symbols_xcode(FILE *input, FILE *output) {
+    int symbol;
+    while ((symbol = fgetc(input)) != EOF) {
+        if (symbol == '\n' || (symbol >= '0' && symbol <= '9')) {
+            fputc(symbol, output);
+            continue;
+        }
+        fprintf(output, "%x", symbol);
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 3 || argc > 4) {
@@ -94,41 +89,114 @@ int main(int argc, char *argv[]) {
         return INVALID_INPUT;
     }
 
-    if (!(argv[1][0] == '-' || argv[1][0] == '/') && argv[1][2] == '\0') {
+    if (!(argv[1][0] == '-' || argv[1][0] == '/') && (argv[1][2] == '\0' || argv[1][3] == '\0')) {
         printf("ERROR: INVALID_FLAG.\n");
         return INVALID_FLAG;
     }
 
-
     char flag;
     bool n_flag = false;
 
-// TODO Скорее-всего, нужно засунуть это в отдельную функцию
     if (argv[1][1] == 'n') {
-        flag = argv[1][2];
-        n_flag = true;
+        if (argc == 4) {
+            flag = argv[1][2];
+            n_flag = true;
+        } else {
+            printf("ERROR: INVALID_INPUT.\n");
+            return INVALID_INPUT;
+        }
     } else {
         flag = argv[1][1];
     }
 
-    switch (flag) {
-        case ('d'):
-            int result = flag_D(argv, n_flag);
-
-            if (result == NOT_EXIST_FILE) {
-                printf("ERROR: NOT_EXIST_FILE. The file with this name does not exist.\n");
-                return NOT_EXIST_FILE;
-            }
-
-//            char str[BUFFER_SIZE];
-//            while (fgets(str, BUFFER_SIZE, input_file) != NULL) {
-//
-//            }
-            break;
-
+    // Открываем input файл
+    FILE *input_file = fopen(argv[2], "r");
+    if (input_file == NULL) {
+        printf("ERROR: NOT_EXIST_FILE. The file with this name does not exist.\n");
+        return NOT_EXIST_FILE;
     }
 
-//    FILE *output_file = NULL;
 
+    FILE *output_file;
+
+    // Открываем output файл (При ошибке закрываем input файл)
+    if (n_flag) {
+        if (strcmp(argv[2], argv[3]) == 0) {
+            fclose(input_file);
+            printf("ERROR: INVALID_FILE. The input file is equal to the output file");
+            return INVALID_FILE;
+        }
+
+        output_file = fopen(argv[3], "w");
+        if (output_file == NULL) {
+            fclose(input_file);
+            printf("ERROR: NOT_EXIST_FILE. The file with this name does not exist.\n");
+            return NOT_EXIST_FILE;
+        }
+
+    } else {
+        // Выделили память - в дальнейшем нужно закрывать
+        char *outfile_path = (char *) malloc((strlen(argv[2]) + 6) * sizeof(char));
+        if (outfile_path == NULL) {
+            fclose(input_file);
+            printf("ERROR: MEMORY_ERROR.\n");
+            return MEMORY_ERROR;
+        }
+        // Возможная ошибка связанная с /. Может быть разделитель: 2x Обратный слеш
+        char *last_slash = strrchr(argv[2], '\\');
+        if (last_slash == NULL) {
+            strcat(outfile_path, "out_");
+            strcat(outfile_path, argv[2]);
+            outfile_path[strlen(argv[2]) + strlen("out_") + 1] = '\0';
+        } else {
+            char *old_name = last_slash + 1;
+            unsigned long long int path_without_name = strlen(argv[2]) - strlen(old_name);
+
+            strncpy(outfile_path, argv[2], path_without_name);
+            outfile_path[path_without_name] = '\000';
+            strcat(outfile_path, "out_");
+            strcat(outfile_path, old_name);
+            outfile_path[strlen(argv[2]) + strlen("out_") + 1] = '\0';
+        }
+
+        output_file = fopen(outfile_path, "w");
+        free(outfile_path);
+
+        if (output_file == NULL) {
+            fclose(input_file);
+            printf("ERROR: NOT_EXIST_FILE. The file with this name does not exist.\n");
+            return NOT_EXIST_FILE;
+        }
+    }
+    switch (flag) {
+        case ('d'):
+            remove_numbers(input_file, output_file);
+            printf("Arabic numerals were excluded.\n"
+                   "The output file was successfully generated\n");
+            break;
+        case ('i'):
+            count_roman_alphabet(input_file, output_file);
+            printf("The number of letters from the Latin alphabet has been counted.\n"
+                   "The output file was successfully generated\n");
+            break;
+        case ('s'):
+            count_specific_symbols(input_file, output_file);
+            printf("The number of letters has been counted.\n"
+                   "The output file was successfully generated\n");
+            break;
+        case ('a'):
+            replace_symbols_xcode(input_file, output_file);
+            printf("The letters were replaced by an ASCII code written in the base 16 numeral system.\n"
+                   "The output file was successfully generated\n");
+            break;
+        default:
+            fclose(input_file);
+            fclose(output_file);
+
+            printf("ERROR: INVALID_FLAG.\n");
+            return INVALID_FLAG;
+    }
     fclose(input_file);
+    fclose(output_file);
+    return OK;
 }
